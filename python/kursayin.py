5fded0dc398f8b0256d22b0965cb69eb0cb281b3
Sergey <shahenbayan@gmail.com>
2022-05-03 22:34:55 +0400

    python

---
+++ python/kursayin.py
@@ -0,0 +1,40 @@
+def snail(matrix):
+    res = []
+    if len(matrix) == 0:
+        return res
+    row_begin = 0
+    row_end = len(matrix) - 1
+    col_begin = 0
+    col_end = len(matrix[0]) - 1
+
+    while row_begin <= row_end and col_begin <= col_end:
+      
+        for i in range(col_end, col_begin-1, -1):
+            res.append(matrix[row_end][i])
+        row_end -= 1
+
+        for i in range(row_end, row_begin-1,-1):
+            res.append(matrix[i][col_begin])
+        col_begin += 1
+
+        if row_begin <= row_end:
+            for i in range(col_begin, col_end+1):
+                res.append(matrix[row_begin][i])
+        row_begin += 1
+
+        if col_begin <= col_end:
+            for i in range(row_begin, row_end+1):
+                res.append(matrix[i][col_end])
+        col_end -= 1
+
+    return res
+
+  
+matrix = [
+  [1,  2 , 3,  4],
+  [6,  7 , 8 , 9], 
+  [11, 12, 13, 14],
+  [16, 17, 18, 19]
+] 
+  
+print(snail(matrix))
